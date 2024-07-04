c88989ce31065648c0412a3463c6daf3f4cd097c
BARBOSA191919 <sbarbosarivas@gmail.com>
2024-07-04 16:25:25 -0500

    first commit

---
+++ administrador/includes/footer.php
@@ -0,0 +1,21 @@
+        <!-- Inclusión de archivos JavaScript esenciales -->
+        <!-- jQuery - biblioteca JavaScript -->
+        <script src="js/jquery-3.3.1.min.js"></script>
+
+        <!-- Bootstrap - framework front-end para estilos y funcionalidades -->
+        <script src="js/bootstrap.min.js"></script>
+
+        <!-- Script principal -->
+        <script src="js/main.js"></script>
+
+        <!-- Font Awesome Kit - iconos -->
+        <script src="https://kit.fontawesome.com/0f12a91c95.js" crossorigin="anonymous"></script>
+
+        <!-- SweetAlert - plugin para crear cuadros de alerta -->
+        <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+
+        <!-- DataTables - plugin para mejorar tablas HTML -->
+        <script type="text/javascript" src="js/plugins/jquery.dataTables.min.js"></script>
+        <script type="text/javascript" src="js/plugins/dataTables.bootstrap.min.js"></script>
+    </body>
+</html>
---
+++ alumno/includes/footer.php
@@ -0,0 +1,17 @@
+    <!-- Inclusión de archivos JavaScript esenciales -->
+    <!-- jQuery - biblioteca JavaScript -->
+    <script src="js/jquery-3.3.1.min.js"></script>
+
+    <!-- Bootstrap - framework front-end para estilos y funcionalidades -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Script personalizado principal -->
+    <script src="js/main.js"></script>
+
+    <!-- Font Awesome Kit - iconos personalizados -->
+    <script src="https://kit.fontawesome.com/0f12a91c95.js" crossorigin="anonymous"></script>
+
+    <!-- SweetAlert - plugin para crear cuadros de diálogo personalizados -->
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+  </body>
+</html>
